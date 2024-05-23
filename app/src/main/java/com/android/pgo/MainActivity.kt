0fac9b3c012fdd604196d4d4e287cfa239dbabf0
krataman <denisekkratochvil@gmail.com>
2024-05-23 10:04:46 +0200

    V 1.1
    - Added Video View to TrimVideoActivity
    - Added button to access gallery of already taken videos

--- app/src/main/java/com/android/pgo/MainActivity.kt
+++ app/src/main/java/com/android/pgo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.android.pgo
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -9,6 +10,7 @@ import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -18,9 +20,11 @@ class MainActivity : AppCompatActivity() {
     private val REQUEST_VIDEO_PERMISSIONS = 2 //static value
     private val APPLICATION_PERMISSIONS = arrayOf(Manifest.permission.CAMERA) // list of permission that the app requires to function properly
 
+    //region onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
 
         initButton()
 
@@ -34,8 +38,11 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
+    //endregion
     //region initialize button
+    /**
+     * Method that initializes the button to take a video
+     */
     private fun initButton(){
         val filmVideoButton:Button = findViewById(R.id.startFilmingVideoButton)
         filmVideoButton.setOnClickListener(View.OnClickListener {
@@ -44,11 +51,19 @@ class MainActivity : AppCompatActivity() {
     }
     //endregion
     //region request permission
+    /**
+     * Method that requests the video permissions from the user
+     */
     private fun requestVideoPermissions() {
         ActivityCompat.requestPermissions(this, APPLICATION_PERMISSIONS, REQUEST_VIDEO_PERMISSIONS)
+
     }
     //endregion
     //region doesAppHave perms?
+    /**
+     * Method that checks if the app has
+     * the required permissions to work properly
+     */
     private fun hasPermissions(permissions: Array<String>): Boolean {
         return permissions.all {
             ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
@@ -56,6 +71,12 @@ class MainActivity : AppCompatActivity() {
     }
     //endregion
     //region onRequestPermissionsResult
+    /**
+     * Method that handles the result of the permission request.
+     * If the user grants the permission, the user will be able to take a video.
+     *
+     * Else the user will be notified that the permissions are required to continue.
+     */
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
@@ -64,13 +85,17 @@ class MainActivity : AppCompatActivity() {
                 if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
                     takeVideoIntent()
                 } else {
-                    Toast.makeText(this, "Permissions to access camera is required!", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Permissions to access the camera is required!", Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
     //endregion
     //region takeVideo
+    /**
+     * Method that starts the Intent to take a video.
+     */
+    @SuppressLint("QueryPermissionsNeeded")
     private fun takeVideoIntent() {
         Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
             takeVideoIntent.resolveActivity(packageManager)?.also {
@@ -80,11 +105,21 @@ class MainActivity : AppCompatActivity() {
     }
     //endregion
     //region onActivityResult
+    /**
+     * Method that handles the result of the video capture.
+     * If the user presses ok, activity TrimVideoActivty will be started.
+     */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
-            val intent:Intent = Intent(this, TrimVideoActivity::class.java)
-            startActivity(intent)
+
+            val videoUri: Uri? = data?.data
+            videoUri?.let {
+                val intent = Intent(this, TrimVideoActivity::class.java).apply {
+                    putExtra("VIDEO_URI", it.toString())
+                }
+                startActivity(intent)
+            }
         }
     }
     //endregion
--- app/src/main/java/com/android/pgo/TrimVideoActivity.kt
+++ app/src/main/java/com/android/pgo/TrimVideoActivity.kt
@@ -1,20 +1,30 @@
 package com.android.pgo
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.VideoView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class TrimVideoActivity : AppCompatActivity() {
+
+    private lateinit var videoView: VideoView
+    private lateinit var videoUri: Uri
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_trim_video)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        enableEdgeToEdge()
+
+        videoView = findViewById(R.id.videoView)
+        val uriString = intent.getStringExtra("VIDEO_URI")
+
+        if (uriString != null) {
+            videoUri = Uri.parse(uriString) // parse do Uri
+            videoView.setVideoURI(videoUri)
+            videoView.start()
         }
     }
 }
